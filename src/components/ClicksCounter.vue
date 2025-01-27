a154000d6e9b1ff02d14533ba647ef5c010c65aa
nyka2002 <146891125+nyka2002@users.noreply.github.com>
2025-01-27 03:41:41 +0100

    Update ClicksCounter.vue

--- src/components/ClicksCounter.vue
+++ src/components/ClicksCounter.vue
@@ -3,6 +3,10 @@
       <button @click="counterStore.increment">click the button!</button>
       <p>clicks count: {{ counterStore.counter }}</p>
       <p>clicks count squared: {{ counterStore.squaredCounter }}</p>
+    
+        <button @click="incrementCounter">button clicker number 2</button>
+        <p>number of clicks  of the second button: {{ counter }}</p>
+        <p>second buton clicks cubed: {{ cubedCounter }}</p>
     </div>
   </template>
   
@@ -10,6 +14,21 @@
   import { useCounterStore } from "../store/counterStore";
 
   export default {
+    data() {
+        return {
+          counter: 0,
+        };
+      },
+    computed: {
+        cubedCounter() {
+          return this.counter * this.counter * this.counter;
+        },
+      },
+      methods: {
+        incrementCounter() {
+          this.counter++;
+        },
+      },
     setup() {
       const counterStore = useCounterStore()
       return { counterStore }
